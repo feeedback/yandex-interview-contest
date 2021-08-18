42e5980cbec22f1233421fc32775d381df818834
A. Rzhanitsyn <risdeep@gmail.com>
2021-08-18 22:39:01 +0500

    add

--- G/index.js
+++ G/index.js
@@ -60,6 +60,85 @@ print(cost[2])
 // const output = fs.createWriteStream(`${__dirname}/output.txt`);
 // const rl = readline.createInterface({ input, terminal: false });
 
+// при условии, что мы имеем дело со смежным списком
+// например, таким: adj = {A:[B,C,D], B:[E,F], ... }
+// const adj = { [1, 2, 3, 4};
+// const graph = {
+//   1: { name: 1 },
+//   2: { name: 2 },
+//   3: { name: 3 },
+//   4: { name: 4 },
+// };
+
+// const adj = {
+//   1: [graph[2], graph[3], graph[4]],
+//   2: [graph[1], graph[3], graph[4]],
+//   3: [graph[1], graph[2], graph[4]],
+//   4: [graph[1], graph[2], graph[3]],
+// };
+// function bfs(toplivo, _adj, _graph, startN, finishN) {
+//   const start = _graph[startN];
+//   const finish = _graph[finishN];
+
+//   // adj - смежный список
+//   // start - начальная вершина
+//   // finish - пункт назначения
+
+//   // инициализируем очередь
+//   const queue = [];
+//   // добавляем s в очередь
+//   queue.push(start);
+//   // помечаем start как посещенную вершину во избежание повторного добавления в очередь
+//   start.visited = true;
+
+//   while (queue.length > 0) {
+//     // удаляем первый (верхний) элемент из очереди
+//     const current = queue.shift();
+//     // adj[current] - соседи current
+
+//     for (const neighbor of _adj[current.name]) {
+//       // if (neighbor.name === current.name) {
+//       //   continue;
+//       // }
+//       console.log(neighbor.name);
+
+//       if (!neighbor.visited) {
+//         queue.push(neighbor); // добавляем его в очередь
+//         neighbor.visited = true; // помечаем вершину как посещенную
+
+//         if (neighbor.name === finish.name) {
+//           // если сосед является пунктом назначения, мы победили
+//           console.log('Нашли финиш', queue);
+//           return true;
+//         }
+//       }
+//     }
+//   }
+//   // если finish не обнаружено, значит пункта назначения достичь невозможно
+//   return -1;
+// }
+// function traverseBFS(root) {
+//   const queue = [root];
+//   const res = [];
+
+//   while (queue.length) {
+//     const curr = queue.shift();
+//     res.push(curr.key);
+
+//     if (curr.right) {
+//       queue.push(curr.right);
+//     }
+
+//     if (curr.left) {
+//       queue.push(curr.left);
+//     }
+//   }
+
+//   return res;
+// }
+
+// bfs(2, adj, graph, 1, 4);
+
 function inputProcessing(lines) {
   // ["4", "0 0", "1 0", "0 1", "1 1", "2", "1 4"]
 
@@ -101,22 +180,38 @@ function inputProcessing(lines) {
 
   console.log({ paths });
 
-  const recur = (current, path, ways, visited = new Map()) => {
+  const visited = new Map();
+
+  const recur = (current, path, ways) => {
     // console.log('new recur', { current, path, ways, visited });
     visited.set(current, true);
 
-    for (let index = 0; index < ways; index++) {
-      if (!visited.get(index)) {
-        console.log({ index, path });
+    const distToFinish = paths[Math.min(current, toCityIndex)][Math.max(current, toCityIndex)];
+    console.log('to finish need', distToFinish, 'but max', maxDistance);
 
-        recur(index, [...path, index], ways - 1, visited);
+    if (distToFinish <= maxDistance) {
+      console.log('finish');
+      console.log('last step', { current, toCityIndex, distToFinish, path });
+      return [...path, toCityIndex];
+    }
+
+    for (let index = 0; index < cityN; index++) {
+      if (index !== toCityIndex && !visited.get(index)) {
+        const dist = paths[Math.min(current, index)][Math.max(current, index)];
+        if (dist <= maxDistance) {
+          console.log({ current, index, dist, path });
+          visited.set(index, true);
+          // console.log(path);
+          return recur(index, [...path, index], ways - 1);
+        }
       }
     }
-    console.log(path);
+    // console.log(path);
+    return -1;
     // recur(current, [...path], ways - 1, visited);
     // console.log('цикл кончился');
   };
-  recur(0, [0], cityN);
+  return recur(fromCityIndex, [fromCityIndex], cityN);
   // const set1 = new Set(lines.slice(1, cityN + 1).map(Number));
 
   // let stoneJewelryCount = 0;
@@ -129,9 +224,14 @@ function inputProcessing(lines) {
   // }
   // return stoneJewelryCount;
 }
-module.exports = inputProcessing;
-// inputProcessing(['4', '0 0', '1 0', '0 1', '1 1', '1', '1 4']);
-inputProcessing(['7', '0 0', '0 2', '2 2', '0 -2', '2 -2', '2 -1', '2 1', '2', '1 3']);
+// console.log(inputProcessing(['4', '0 0', '1 1', '1 0', '1 2', '1', '1 4']));
+// [
+//   [0, 1, 1, 2],
+//   [0, 0, 2, 1],
+//   [0, 0, 0, 1],
+//   [0, 0, 0, 0],
+// ];
+console.log(inputProcessing(['7', '0 0', '0 1', '2 2', '0 -2', '2 -2', '2 -1', '2 1', '2', '1 3']));
 // [
 //   [0, 1, 1, 2],
 //   [0, 0, 2, 1],
@@ -141,6 +241,15 @@ inputProcessing(['7', '0 0', '0 2', '2 2', '0 -2', '2 -2', '2 -1', '2 1', '2', '
 // [
 //   [0, 2, 4, 2, 4, 3, 3],
 //   [0, 0, 2, 4, 6, 5, 3],
+//   [0, 0, 0, 6, 4, 3, 1],
+//   [0, 0, 0, 0, 2, 3, 5],
+//   [0, 0, 0, 0, 0, 1, 3],
+//   [0, 0, 0, 0, 0, 0, 2],
+//   [0, 0, 0, 0, 0, 0, 0],
+// ];
+// [
+//   [0, 1, 4, 2, 4, 3, 3],
+//   [0, 0, 3, 3, 5, 4, 2],
 //   [0, 0, 0, 6, 4, 3, 1],
 //   [0, 0, 0, 0, 2, 3, 5],
 //   [0, 0, 0, 0, 0, 1, 3],
