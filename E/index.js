7ff641bd8f9a88b0865b18eee4535933fd65be5b
A. Rzhanitsyn <risdeep@gmail.com>
2021-08-20 15:16:12 +0500

    fix test E,G

--- E/index.js
+++ E/index.js
@@ -1,52 +1,38 @@
-const readline = require('readline');
-const fs = require('fs');
+function dictFromString(str = '') {
+  const dict = new Map();
 
-module.exports = () => {
-  // delete upper line exports for contest
-  const input = fs.createReadStream(`${__dirname}/input.txt`);
-  const output = fs.createWriteStream(`${__dirname}/output.txt`);
-  const rl = readline.createInterface({ input, terminal: false });
-
-  function dictFromString(str = '') {
-    const dict = new Map();
-
-    for (const char of str) {
-      dict.set(char, dict.get(char) + 1 || 1);
-    }
-
-    return dict;
+  for (const char of str) {
+    dict.set(char, dict.get(char) + 1 || 1);
   }
 
-  function areAnagrams(str1 = '', str2 = '') {
-    if (str1.length !== str2.length) {
-      return 0;
-    }
+  return dict;
+}
 
-    const dict1 = dictFromString(str1);
-    const dict2 = dictFromString(str2);
-
-    if (dict1.size !== dict2.size) {
-      return 0;
-    }
-
-    for (const [char, dict1Count] of dict1) {
-      if (dict1Count !== dict2.get(char)) {
-        return 0;
-      }
-    }
-
-    return 1;
+function areAnagrams(str1 = '', str2 = '') {
+  if (str1.length !== str2.length) {
+    return 0;
   }
 
-  const lines = [];
+  const dict1 = dictFromString(str1);
+  const dict2 = dictFromString(str2);
 
-  rl.on('line', (line) => {
-    lines.push(line);
-  });
+  if (dict1.size !== dict2.size) {
+    return 0;
+  }
 
-  rl.on('close', () => {
-    const [str1, str2] = lines;
+  for (const [char, dict1Count] of dict1) {
+    if (dict1Count !== dict2.get(char)) {
+      return 0;
+    }
+  }
 
-    output.write(String(areAnagrams(str1, str2)));
-  });
-};
+  return 1;
+}
+
+function inputProcessing(lines) {
+  const [str1, str2] = lines;
+
+  return areAnagrams(str1, str2);
+}
+
+module.exports = inputProcessing;
--- E/index.test.js
+++ E/index.test.js
@@ -1,28 +1,13 @@
-const testData = [
-  [['friend', 'finder'], '1'],
-  [['cat', 'car'], '0'],
-  [['af', 'cd'], '0'],
-];
-
-const fs = require('fs');
-const path = require('path');
 const inputProcessing = require('./index');
 
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-afterAll(async () => {
-  fs.promises.unlink(path.join(__dirname, 'input.txt')).catch();
-  fs.promises.unlink(path.join(__dirname, 'output.txt')).catch();
-});
+const testData = [
+  [['friend', 'finder'], 1],
+  [['cat', 'car'], 0],
+  [['af', 'cd'], 0],
+];
 
 testData.forEach(([input, output], index) => {
-  test(`${index + 1}`, async () => {
-    fs.writeFileSync(path.join(__dirname, 'input.txt'), input.join('\n'));
-
-    inputProcessing();
-    await wait(50);
-
-    const outputFromFile = String(fs.readFileSync(path.join(__dirname, 'output.txt'), 'utf-8'));
-    expect(outputFromFile).toStrictEqual(output);
+  test(`${index + 1}`, () => {
+    expect(inputProcessing(input)).toStrictEqual(output);
   });
 });
